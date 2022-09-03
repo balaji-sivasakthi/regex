e665aa465af44711b7997d1d5272416cb4ffcbbc
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2022-09-03 23:24:47 +0530

    regex file

---
+++ main.js
@@ -0,0 +1,6 @@
+const regex_logo = document.getElementById('regex_logo');
+regex_logo.addEventListener('mouseover',(e)=>{
+    console.log("Hello, World!");
+    regex_logo.style.translate= e.target.clientX
+    // regex_logo.style.position=e.target.
+})
