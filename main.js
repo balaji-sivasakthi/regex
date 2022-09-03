e8d1caf455d741700c21d11dfd61d57247eaab85
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2022-09-03 23:53:30 +0530

    😂 funny animation for regex logo

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const regex_logo = document.getElementById('regex_logo');
 regex_logo.addEventListener('mouseover',(e)=>{
     console.log("Hello, World!");
-    regex_logo.style.translate= e.target.clientX
-    // regex_logo.style.position=e.target.
+    console.log(e);
+    console.log(e.pageX/100);
+    regex_logo.style.transform= `rotateY(${e.pageX}deg)`;
+    // regex_logo.style.position=e.target.  
 })
