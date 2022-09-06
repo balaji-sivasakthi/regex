06c9056908b728d6f8ea3f5d232b12c7602c3a37
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2022-09-06 09:24:48 +0530

    done

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-const regex_logo = document.getElementById('regex_logo');
-regex_logo.addEventListener('mouseover',(e)=>{
-    console.log("Hello, World!");
-    console.log(e);
-    console.log(e.pageX/100);
-    regex_logo.style.transform= `rotateY(${e.pageX}deg)`;
-    // regex_logo.style.position=e.target.  
-})
+    
